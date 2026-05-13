658584c11ff8fef8468fef20849fb8d4a9d8bf93
Manuel Gajoni <mgajoni@gmail.com>
2026-05-13 14:42:17 +0200

    Primo commit: setup iniziale Spring Boot

---
+++ src/test/java/com/esame/EsameReact_be/EsameReactBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esame.EsameReact_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsameReactBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
